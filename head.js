1cccdbf04907d41f2d39ec75243263446815d515
Lliam McBean <lliam.mcbean@gmail.com>
2021-07-19 22:42:24 +0000

    commiting more stuff

--- head.js
+++ head.js
@@ -1,19 +1,19 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-        console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-    } else {
-        console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-    }
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
 };
   
 //HEAD FUNCTION
 const head = (arr) => {
-    if(arr.length === 0) {
-        return undefined
-    }
-    return arr[0]
-}
+  if (arr.length === 0) {
+    return undefined;
+  }
+  return arr[0];
+};
 
 // TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
