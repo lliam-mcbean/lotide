6e329bd4b3a9528d3d770311552180171e47ecd1
Lliam McBean <lliam.mcbean@gmail.com>
2021-07-21 21:12:11 +0000

    finished countLetters exercise

---
+++ countLetters.js
@@ -0,0 +1,13 @@
+const countLetters = function(str) {
+    let count = {};
+    for(let i = 0; i < str.length; i++) {
+        if(count[str[i]]) {
+            count[str[i]] += 1;
+        } else {
+            count[str[i]] = 1;
+        }
+    }
+    return count;
+}
+
+console.log(countLetters('hellobabybird'));
