eb4d22029d18b4a36f9fb8f198182758bd450aeb
Lliam McBean <lliam.mcbean@gmail.com>
2021-07-21 00:09:08 +0000

    had to make a tweat to arrayasserequal function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,11 +1,11 @@
 const assertArraysEqual = (arr1, arr2) => {
     if(arr1.length != arr2.length) {
-        return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+        return `🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`;
     }
     for(let i = 0; i < arr1.length; i++) {
         if(arr1[i] !== arr2[i]) {
-            return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+            return `🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`;
         }
-        return `✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+        return `✅✅✅ Assertion Passed: ${arr1} === ${arr2}`;
     }
 }
